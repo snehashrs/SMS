e493331de84da2283c707a9ac6f131d3304a743e
Sneha <snheha123@gmail.com>
2025-01-14 10:45:00 +0545

    Added getter methods to Student class.

--- src/week8/Student.java
+++ src/week8/Student.java
@@ -1,5 +1,43 @@
 package week8;
 
 public class Student {
+	private String name;
+	private int age;
+	
+	public Student (String name, int age){
+		this.name = name;
+		this.age = age;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
+
+
+
+
+//u-1, scanner, operator
+//encapsulation(why use, geter, setter, solve)int void
+//testing (assert types, @test, beforetest)after beforeall
+//abstraction(method,  kun class= abstartct, code, which error)
+//code tick
+//interface public abstarct choosing keyword
+//development cycle
+//javadocs ()
+//brancing/straging (U-untract, a- tract files, s-)
+//filehandein, delete, read write method, which error, exeption, file exception
+//code, file create, delete, classify
+//solid- code all principles (2,3)
